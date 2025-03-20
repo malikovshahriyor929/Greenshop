880e8cc3102545344396874cbc078338d889a2e6
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-03-20 12:59:06 +0500

    first

--- src/components/cardView/index.tsx
+++ src/components/cardView/index.tsx
@@ -18,14 +18,14 @@ const CardViewComponents = () => {
   console.log(data);
 
   return (
-    <div className="w-[90%] mx-auto max-w-[1440px]">
-      <p className="text-[#3d3d3d] ">Home / Shop</p>
-      <div className="grid grid-cols-2 max-[910px]:grid-cols-1 max-[1100px]:grid-cols-[2fr_1.7fr] max-[1020px]:grid-cols-[2fr_1.5fr] max-[1000px]:grid-cols-[2fr_1.3fr] my-5 max-[1233px]:gap-5 ">
-        <div className="flex  gap-10 max-[1233px]:gap-4 ">
-          <div className="grid grid-cols-1 items-center gap-4 max-[995px]:gap-0 max-[995px]:  ">
+    <div className="w-[90%] mx-auto max-w-[1440px] max-[600px]:mb-20">
+      <p className="text-[#3d3d3d] mt-5 ">Home / Shop</p>
+      <div className="grid grid-cols-2 max-[827px]:grid-cols-1 max-[1100px]:grid-cols-[2fr_1.7fr] max-[1020px]:grid-cols-[2fr_1.5fr] max-[1000px]:grid-cols-[2fr_1.3fr] my-5 max-[1233px]:gap-5 items-start ">
+        <div className="flex max-[910px]:flex-col-reverse  max-[910px]:gap-5 gap-10 max-[1233px]:gap-4 ">
+          <div className="grid grid-cols-1  max-[910px]:grid-cols-4 items-center gap-4 max-[995px]:gap-0 max-[995px]:  ">
             {data?.detailed_images.map((value: string, idx: number) => (
               <Image
-                className="!w-[100px] !h-[100px] max-[99px]:!h-[70px] max-[99px]:!w-[70px] !object-cover bg-[#fbfbfb] rounded-lg "
+                className="!w-[100px] !h-[100px] max-[475px]:mx-auto max-[475px]:!h-[70px] max-[475px]:!w-[70px] max-[370px]:!h-[60px] max-[370px]:!w-[60px]  !object-cover bg-[#fbfbfb] rounded-lg "
                 key={idx}
                 src={value}
                 alt=""
@@ -34,16 +34,16 @@ const CardViewComponents = () => {
           </div>
           <div className=" **:!rounded-lg mx-auto   max-[9]: p-2 *:!rounded-lg !max-w-[400px] object-contain  !max-h-[400px]">
             <Image
-              className="!w-full !h-full bg-[#fbfbfb] max-[910px]:!max-w-[400px] max-[910px]:!max-h-[400px]  !p-2  !rounded-lg !object-cover "
+              className="!w-full !h-full bg-[#fbfbfb] ax-[910px]:!max-w-[400px] max-[910px]:!max-h-[400px]  !p-2  !rounded-lg !object-cover "
               src={data?.main_image}
             />
           </div>
         </div>
         <div>
-          <h2 className="text-[#3d3d3d] text-[28px] font-bold">
+          <h2 className="text-[#3d3d3d] text-[28px] font-bold max-[300px]:text-[24px]">
             {data?.title}
           </h2>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between max-[356px]:flex-col max-[356px]:items-start">
             <p className="text-[#46a358] text-[22px] font-bold">
               ${data?.price}
             </p>
@@ -90,7 +90,7 @@ const CardViewComponents = () => {
                 <MinusOutlined />
               </div>
             </div>
-            <div className="w-fit flex  max-[1100px]:!p-[5px_10px] items-center gap-5 max-[1059px]:gap-3">
+            <div className="w-fit flex   items-center gap-5 max-[1059px]:gap-3 max-[305px]:gap-1 grid-cols-2">
               <Button
                 x={15}
                 y={10}
@@ -101,7 +101,7 @@ const CardViewComponents = () => {
               <button className="border-[#46a358] border text-nowrap   max-[1059px]:!p-[5px_10px] rounded-lg px-5 py-2.5 flex  gap-1 text-[#46a358] ">
                 Add to cart
               </button>
-              <button className="border-[#46a358] border  max-[1059px]:!p-[5px_10px] rounded-lg p-2 flex  gap-1 text-[#46a358] ">
+              <button className="border-[#46a358] w-fit border  max-[1059px]:!p-[5px_10px] rounded-lg p-2 flex  gap-1 text-[#46a358] ">
                 <BiHeart size={25} />
               </button>
             </div>
@@ -127,7 +127,7 @@ const CardViewComponents = () => {
             className="flex items-center gap-2 my-4
            "
           >
-            <p>Share this products:</p>
+            <p className="max-[300px]:text-[14px]">Share this products:</p>
             <TiSocialFacebook size={25} />
             <FaTwitter />
             <FaLinkedinIn size={20} />
@@ -140,29 +140,3 @@ const CardViewComponents = () => {
 };
 
 export default CardViewComponents;
-
-{
-  category: "house-plants";
-  comments: [];
-  created_at: "2024-08-22T03:22:18.963Z";
-  created_by: "66c48eb24d966b830dd5925e";
-  description: "<h4>About Monstera Deliciosa</h4>\n<p>Monstera Deliciosa, commonly referred to as the Swiss Cheese Plant due to its distinctive leaf patterns, is a staple in many plant lovers' collections. This stunning plant originates from the tropical rainforests of Central America, where it climbs trees and absorbs nutrients from the air and soil.</p>\n<p>The Monstera’s leaves are its most striking feature. When young, the leaves are heart-shaped and solid. As the plant matures, the leaves become large and develop characteristic splits and holes, creating the iconic look that many plant enthusiasts adore. These holes and splits are thought to help the plant survive in its native habitat by allowing strong winds to pass through the leaves without causing damage.</p>\n<p>This plant is relatively easy to care for, making it ideal for both beginners and experienced gardeners. It prefers bright, indirect sunlight but can also tolerate lower light conditions. However, the more light it receives, the more dramatic the leaf fenestrations will be. Water the Monstera when the top inch of soil feels dry to the touch. It’s essential to avoid overwatering, as the plant is susceptible to root rot if left sitting in waterlogged soil.</p>\n<p>Monstera Deliciosa can grow quite large, reaching several feet in height if given the right conditions. It’s a slow to moderate grower, so patience is key. You can train the plant to climb a moss pole or trellis, mimicking its natural growth habit. The plant’s aerial roots can be tucked into the soil or wrapped around the support structure to help it climb.</p>\n<p>In addition to its aesthetic appeal, the Monstera Deliciosa is also known for its air-purifying qualities. It helps filter toxins from the air, making your home environment healthier. However, it’s important to note that the plant is toxic if ingested, so it should be kept out of reach of pets and small children.</p>\n<p>Overall, Monstera Deliciosa is a versatile, low-maintenance plant that can transform any indoor space into a tropical oasis. Its striking leaves and impressive size make it a focal point in any room, while its easy care requirements ensure that it remains a beloved member of your plant collection for years to come.</p>\n";
-  detailed_images: [
-    "http://res.cloudinary.com/dj28bsagl/image/upload/v1724296909/bhot2xhirbcefeubynnu.webp",
-    "http://res.cloudinary.com/dj28bsagl/image/upload/v1724296912/qpbfxo5e5nerxktjttsd.webp",
-    "http://res.cloudinary.com/dj28bsagl/image/upload/v1724296915/u9rrwdfsz4tivlfrvqer.webp",
-    "http://res.cloudinary.com/dj28bsagl/image/upload/v1724296917/lh0m248ekoquq1jgxrfi.webp",
-  ];
-  discount: true;
-  discount_price: "15";
-  main_image: "http://res.cloudinary.com/dj28bsagl/image/upload/v1724296904/njkema9dymtyhnyarh1p.webp";
-  price: 35.99;
-  rate: 0;
-  short_description: 'Monstera Deliciosa, often known as the "Swiss Cheese Plant," is a tropical houseplant prized for its large, glossy leaves that develop unique splits and holes as they mature. Originating from the rainforests of Central America, this plant brings a touch of the exotic into any indoor space. It thrive';
-  sold_times: 0;
-  tags: [];
-  title: "Monstera Deliciosa";
-  views: 0;
-  __v: 0;
-  _id: "66c6aeea88d60e51851abf09";
-}
