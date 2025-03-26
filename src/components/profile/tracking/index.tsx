07e943e6e1f045a699a234e30232221e394d1fff
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-03-26 15:47:47 +0500

    first

--- src/components/profile/tracking/index.tsx
+++ src/components/profile/tracking/index.tsx
@@ -1,12 +1,13 @@
 import useQueryHandler from "../../../hooks/useQueryHandler";
 import CookiesInfo from "../../../shared/generics/cookie";
+import { cardSkeleton } from "../../../shared/generics/loader";
 import OrderCard from "./order";
 
 const Tracking = () => {
   // let [myData, setMyData] = useState<CardType[]>([]);
   let { getCookie } = CookiesInfo();
   let userInfo = getCookie("user");
-  let { data } = useQueryHandler({
+  let { data, isLoading } = useQueryHandler({
     pathname: "orders",
     url: "order/get-order",
   });
@@ -20,11 +21,17 @@ const Tracking = () => {
 
   return (
     <div className="grid grid-cols-3 my-5 gap-5  max-[1130px]:grid-cols-2 max-[532px]:grid-cols-1 max-[340px]:grid-cols-1">
-      {mydata?.slice(0, 10)?.map((value, idx) => (
-        <div key={idx} className="h-full w-full">
-          <OrderCard props={value} />
-        </div>
-      ))}
+      {isLoading
+        ? Array?.from({ length: 6 }).map((_, idx) => (
+            <div className="" key={idx}>
+              {cardSkeleton()}
+            </div>
+          ))
+        : mydata?.slice(0, 10)?.map((value, idx) => (
+            <div key={idx} className="h-full w-full">
+              <OrderCard props={value} />
+            </div>
+          ))}
     </div>
   );
 };
